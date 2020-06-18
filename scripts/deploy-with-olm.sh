f6ca7748110dbf26dd42bfcf008ab1276a3d660a
Dan Prince <dprince@redhat.com>
2020-06-18 18:59:05 -0400

    fixes to scripts/deploy-with-olm.sh

--- scripts/deploy-with-olm.sh
+++ scripts/deploy-with-olm.sh
@@ -3,6 +3,7 @@ set -x
 INDEX_IMAGE=${INDEX_IMAGE:-"quay.io/openstack-k8s-operators/openstack-operators-index:v0.0.1"}
 CSV_VERSION=${CSV_VERSION:-"0.0.1"}
 
+if [ `oc get catalogsource -n "${TARGET_NAMESPACE}" --no-headers 2> /dev/null | grep openstack | wc -l` -eq 0 ]; then
 echo "Creating CatalogSource"
         cat <<EOF | oc create -f -
 apiVersion: operators.coreos.com/v1alpha1
@@ -14,6 +15,7 @@ spec:
   sourceType: grpc
   image: $INDEX_IMAGE
 EOF
+fi
 
 echo "Wait for the catalogSource to be available"
 oc wait deploy "openstack-index" --for condition=available -n openstack --timeout="360s"
@@ -24,8 +26,8 @@ for i in $(seq 1 $RETRIES); do
     oc get packagemanifest -n "openstack" "openstack-cluster" && break
     sleep $i
     if [ "$i" -eq "${RETRIES}" ]; then
-	    echo "packagemanifest 'openstack-cluster' was never created in namespace 'openstack'"
-	    exit 1
+      echo "packagemanifest 'openstack-cluster' was never created in namespace 'openstack'"
+      exit 1
     fi
 done
 
@@ -36,15 +38,15 @@ apiVersion: operators.coreos.com/v1
 kind: OperatorGroup
 metadata:
   name: "openstack-group"
-    namespace: "openstack"
-    spec:
+  namespace: "openstack"
+spec:
   targetNamespaces:
   - "openstack"
 EOF
 fi
 
 echo "Creating Subscription"
-        cat <<EOF | oc create -f -
+cat <<EOF | oc create -f -
 apiVersion: operators.coreos.com/v1alpha1
 kind: Subscription
 metadata:
