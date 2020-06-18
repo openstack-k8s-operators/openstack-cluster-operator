e6cb606837c0ec48aac3bb557276426b10f11581
Dan Prince <dprince@redhat.com>
2020-06-18 19:55:55 -0400

    Add external/internal image workaround to create-local-index.sh

--- scripts/create-local-index.sh
+++ scripts/create-local-index.sh
@@ -4,6 +4,7 @@
 # NOTE: this script assumes you have configured your OpenShift registry with a default route so that it is easy to push images locally
 set -ex
 BUNDLE_IMAGE=${BUNDLE_IMAGE:?"Please define a bundle image."}
+BUNDLE_VERSION=${BUNDLE_VERSION:-"0.0.1"}
 INDEX_VERSION=${INDEX_VERSION:-"0.0.1"}
 PROJECT=${PROJECT:-"openstack"}
 IMAGE_REGISTRY=$(oc get route -n openshift-image-registry -o json | jq ".items[0].spec.host" -r)
@@ -14,6 +15,10 @@ ACCOUNT=$(oc get secret | grep builder-dockercfg | cut -f 1 -d ' ')
 TOKEN=$(oc get secret $ACCOUNT -o json | jq '.metadata.annotations["openshift.io/token-secret.value"]' -r)
 echo "$TOKEN" | podman login -u $ACCOUNT --password-stdin --tls-verify=false $IMAGE_REGISTRY
 
-opm index add --bundles $BUNDLE_IMAGE --tag $IMAGE_REGISTRY/$PROJECT/openstack-operators-index:v$INDEX_VERSION -c podman
-
+#FIXME: need a better way to reconcile the internal and external image names from openshift
+# the opm command below when executed outside the cluster requires access to the bundle image on the CLI
+# but when deployed the database needs to have the internal location recorded
+opm index add --generate --bundles $BUNDLE_IMAGE --tag $IMAGE_REGISTRY/$PROJECT/openstack-operators-index:v$INDEX_VERSION -c podman
+sqlite3 database/index.db "UPDATE operatorbundle SET bundlepath=\"image-registry.openshift-image-registry.svc:5000/$PROJECT/openstack-cluster-bundle:v$BUNDLE_VERSION\"" ".exit"
+buildah bud -t $IMAGE_REGISTRY/$PROJECT/openstack-operators-index:v$INDEX_VERSION index.Dockerfile
 podman push --tls-verify=false $IMAGE_REGISTRY/$PROJECT/openstack-operators-index:v$INDEX_VERSION
