9bddc108d4e9076c4766de0214c6827e5a510b76
Dan Prince <dprince@redhat.com>
2020-06-18 17:48:14 -0400

    Fix $PROJECT on bundle --tag command

--- scripts/create-local-bundle.sh
+++ scripts/create-local-bundle.sh
@@ -13,7 +13,7 @@ ACCOUNT=$(oc get secret | grep builder-dockercfg | cut -f 1 -d ' ')
 TOKEN=$(oc get secret $ACCOUNT -o json | jq '.metadata.annotations["openshift.io/token-secret.value"]' -r)
 echo "$TOKEN" | podman login -u $ACCOUNT --password-stdin --tls-verify=false $IMAGE_REGISTRY
 
-opm alpha bundle build -d deploy/olm-catalog/openstack-cluster/$BUNDLE_VERSION/ --package openstack-cluster --channels beta --default beta --tag $IMAGE_REGISTRY/openstack-cluster-bundle:v$BUNDLE_VERSION -b buildah
+opm alpha bundle build -d deploy/olm-catalog/openstack-cluster/$BUNDLE_VERSION/ --package openstack-cluster --channels beta --default beta --tag $IMAGE_REGISTRY/$PROJECT/openstack-cluster-bundle:v$BUNDLE_VERSION -b buildah
 podman push $IMAGE_REGISTRY/$PROJECT/openstack-cluster-bundle:v$BUNDLE_VERSION
 opm alpha bundle validate --tag $IMAGE_REGISTRY/$PROJECT/openstack-cluster-bundle:v$BUNDLE_VERSION -b podman
 rm bundle.Dockerfile
