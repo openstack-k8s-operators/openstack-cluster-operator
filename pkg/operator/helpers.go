db70e5f3da8fcfb8a2fd0fc05e96b47832cf6b4e
Dan Prince <dprince@redhat.com>
2020-10-08 09:19:07 -0400

    Add dependencies for performance and sriov

--- pkg/operator/helpers.go
+++ pkg/operator/helpers.go
@@ -207,6 +207,30 @@ func getOperatorRules() *[]rbacv1.PolicyRule {
 				"*",
 			},
 		},
+		{
+			APIGroups: []string{
+				"sriovnetwork.openshift.io",
+			},
+			Resources: []string{
+				"*",
+				"sriovnetworks",
+			},
+			Verbs: []string{
+				"*",
+			},
+		},
+		{
+			APIGroups: []string{
+				"performanceprofiles.performance.openshift.io",
+			},
+			Resources: []string{
+				"*",
+				"performanceprofiles",
+			},
+			Verbs: []string{
+				"*",
+			},
+		},
 	}
 }
 
